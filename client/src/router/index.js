feb0b8130d3b7244c3a6efa992fa66ddff58eed4
Floris <kappenfloris@gmail.com>
2020-01-06 19:44:11 +0100

    Change up routes to include top menu component

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,20 +2,28 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Loading from '../views/Loading.vue'
 import Main from '../views/Main.vue'
+import Navigator from '../components/TopMenu.vue'
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "loading",
-    component: Loading
+    name: "main",
+    component: Navigator,
+    children: [
+      {
+        path: "/:roomID",
+        name: "app",
+        component: Main
+      },
+      {
+        path: '/',
+        name: 'loading',
+        component: Loading
+      }
+    ]
   },
-  {
-    path: "/:roomID",
-    name: "app",
-    component: Main
-  }
 ];
 
 const router = new VueRouter({
